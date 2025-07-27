e707d5a7e81d206fdb2ae350bf0b3976daf56970
ayushjoshi45 <ayushjoshi3725@gmail.com>
2025-07-27 22:57:52 +0530

    Setup shadCn ui and tailwind with vite

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js"
 import companyRoute from "./routes/company.route.js"
+import jobRoute from "./routes/job.route.js"
+import appllicationRoute from "./routes/application.route.js"
 dotenv.config({});
 const app=express();
 
@@ -21,8 +23,10 @@ app.use(cors(corsOption));
 // API 
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
+app.use("/api/v1/job",jobRoute);
+app.use("/api/v1/application",appllicationRoute);
 
-const PORT=process.env.PORT || 3000
+const PORT=process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`Server Running on Port ${PORT}`);
     connectDB();
